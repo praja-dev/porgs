3a8854d74d7e98456789924b3df449bc687aae02
Nalaka Jayasena <437578+nalaka@users.noreply.github.com>
2024-07-09 14:38:06 -0700

    Serve static assets at /a/ and add basic app styling
    - Added foundation.css to reset default styles and set style foundation.
    - Added app.css for application level styles.
    - Link to app.css from default.go.html
    - Embedded all files in assets directory
    - Added allow rule in .gitignore for assets/*

--- cmd/porgs/handler.go
+++ cmd/porgs/handler.go
@@ -1,9 +1,22 @@
 package main
 
-import "net/http"
+import (
+	"io/fs"
+	"net/http"
+)
 
 func getHandler() (http.Handler, error) {
 	handler := http.NewServeMux()
+
+	// # Serve assets at /a/
+	assetsDir, err := fs.Sub(embeddedFS, "assets")
+	if err != nil {
+		return nil, err
+	}
+	assetsHandler := http.StripPrefix("/a/", http.FileServer(http.FS(assetsDir)))
+	handler.Handle("/a/", assetsHandler)
+
 	handler.Handle("/{$}", handleRoot())
+
 	return handler, nil
 }
--- cmd/porgs/var.go
+++ cmd/porgs/var.go
@@ -2,6 +2,7 @@ package main
 
 import "embed"
 
+//go:embed assets/*
 //go:embed layouts/*.go.html
 //go:embed views/*.go.html
 var embeddedFS embed.FS
