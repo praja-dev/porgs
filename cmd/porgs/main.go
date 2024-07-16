e803a48cb3c021ed9cd454e4278078771cc9ea98
Nalaka Jayasena <437578+nalaka@users.noreply.github.com>
2024-07-15 21:05:44 -0700

    Initialize SQLite db using eatonphil/gosqlite driver

--- cmd/porgs/main.go
+++ cmd/porgs/main.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"embed"
 	"errors"
+	"github.com/eatonphil/gosqlite"
 	"github.com/praja-dev/porgs"
 	"github.com/praja-dev/porgs/task"
 	"html/template"
@@ -27,25 +28,32 @@ func main() {
 	porgs.Templates = getTemplates()
 	porgs.Handler = getHandlers()
 
+	initDB()
 	run(context.Background())
 }
 
 func getBootConfig() porgs.AppBootConfig {
-	host := os.Getenv("HOST")
-	portStr := os.Getenv("PORT")
+	host := os.Getenv("PORGS_HOST")
+
+	portStr := os.Getenv("PORGS_PORT")
 	if portStr == "" {
 		portStr = "8642"
 	}
-
 	port, err := strconv.Atoi(portStr)
 	if err != nil {
 		slog.Error("config: port", "err", err)
 		os.Exit(1)
 	}
 
+	dsn := os.Getenv("PORGS_DSN")
+	if dsn == "" {
+		dsn = "porgs.db"
+	}
+
 	return porgs.AppBootConfig{
 		Host: host,
 		Port: port,
+		DSN:  dsn,
 	}
 }
 
@@ -157,6 +165,46 @@ func getAssetHandler(plugin porgs.Plugin) http.Handler {
 	assetsHandler := http.StripPrefix("/a/"+plugin.GetName(), http.FileServer(http.FS(assetsDir)))
 
 	return assetsHandler
+}
+
+func initDB() {
+	slog.Info("db: initializing")
+	conn, err := gosqlite.Open(porgs.BootConfig.DSN)
+	if err != nil {
+		slog.Error("db: open", "err", err)
+		os.Exit(1)
+	}
+	defer func() { _ = conn.Close() }()
+	conn.BusyTimeout(3 * time.Second)
+	slog.Info("db: connected", "dsn", porgs.BootConfig.DSN)
+
+	// # Check if user table exists
+	qryUserTbl := "SELECT name FROM sqlite_master WHERE type='table' AND name='user';"
+	stmt, err := conn.Prepare(qryUserTbl)
+	if err != nil {
+		slog.Error("db statement: prepare", "err", err)
+		os.Exit(1)
+	}
+	defer func() { _ = stmt.Close() }()
+	hasRow, err := stmt.Step()
+	if err != nil {
+		slog.Error("db statement: step", "err", err)
+		os.Exit(1)
+	}
+	if hasRow {
+		slog.Info("db: ready")
+		return
+	}
+
+	// # Create user table
+	slog.Info("db: preparing for first use")
+	qryCreateUserTbl := "CREATE TABLE user (id INTEGER PRIMARY KEY, name TEXT);"
+	err = conn.Exec(qryCreateUserTbl)
+	if err != nil {
+		slog.Error("db: create user table", "err", err)
+		os.Exit(1)
+	}
+	slog.Info("db: ready")
 }
 
 func run(ctx context.Context) {
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/praja-dev/porgs
 
 go 1.22.5
+
+require github.com/eatonphil/gosqlite v0.9.0
