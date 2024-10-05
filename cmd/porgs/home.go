f3cee7f8cff85db8607a21471483a3f9e614fbe0
Nalaka Jayasena <437578+nalaka@users.noreply.github.com>
2024-10-04 18:40:56 -0700

    List of root URLs contributed by plugins at /home

--- cmd/porgs/home.go
+++ cmd/porgs/home.go
@@ -5,11 +5,19 @@ import (
 	"net/http"
 )
 
+// vmHome is the view model for the Home screen
+type vmHome struct {
+	Plugins map[string]porgs.Plugin
+}
+
 func handleHome() http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		porgs.RenderView(w, r, porgs.View{
 			Name:  "main-home",
 			Title: "Home | " + porgs.SiteConfig.Title,
+			Data: vmHome{
+				Plugins: porgs.Plugins,
+			},
 		})
 	})
 }
