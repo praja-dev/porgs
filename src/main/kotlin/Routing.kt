f0631ad9fa4bc5ba3d981ce4f99cf28a94045489
Nalaka Jayasena <437578+nalaka@users.noreply.github.com>
2026-04-04 16:30:46 +0530

    feat(ui): load CSS and JS as static resources

--- src/main/kotlin/Routing.kt
+++ src/main/kotlin/Routing.kt
@@ -1,11 +1,15 @@
 package dev.praja
 
 import io.ktor.server.application.*
+import io.ktor.server.http.content.*
 import io.ktor.server.routing.*
 import io.ktor.server.thymeleaf.*
 
 fun Application.configureRouting() {
     routing {
+        staticResources("/css", "css")
+        staticResources("/javascript", "javascript")
+
         get("/") {
             call.respondTemplate("index", mapOf("message" to "porgs: 0.0.1"))
         }
